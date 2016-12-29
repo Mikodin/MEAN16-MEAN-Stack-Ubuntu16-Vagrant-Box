95149e5aee4e9239865a7e18c603210979e8f6ac
Michael Alicea <mfalicea58@gmail.com>
2016-12-28 21:36:06 -0500

    Removal of chef and it's cookbooks

--- cookbooks/mongodb/test/integration/mms_monitoring_agent/inspec/config_server_spec.rb
+++
@@ -1,9 +0,0 @@
-describe service('mongodb-mms-monitoring-agent') do
-  it { should be_installed }
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe file('/etc/mongodb-mms/monitoring-agent.config') do
-  its('content') { should match(/(sslRequireValidServerCertificates=false)/) }
-end
